d208a4244f63bcf73e063bd6f3bc5a0db839e298
shreyansh2310 <sg275527109@gmail.com>
2022-01-26 17:26:21 +0530

    done notes

--- mymate/dashboard/models.py
+++ mymate/dashboard/models.py
@@ -1,4 +1,7 @@
+from collections import deque
+from pyexpat import model
 from tkinter import CASCADE
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -15,3 +18,19 @@ class Notes(models.Model):
     class Meta:
         verbose_name = 'Notes'
         verbose_name_plural = 'Notes'
+
+
+class Homework(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    subject=models.CharField(max_length=100)
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    due=models.DateTimeField()
+    is_finish=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'homework'
+        verbose_name_plural = 'homework'
--- mymate/dashboard/views.py
+++ mymate/dashboard/views.py
@@ -1,12 +1,39 @@
-from django.shortcuts import render
+from pyexpat import model
+from django.shortcuts import render,redirect
 from . forms import *
+from django.contrib import messages
+from django.views import generic
+
 # Create your views here.
 def home(request):
     return render(request,'dashboard/home.html')
 
 
 def notes(request):
-    form=Notesform()
+    if request.method=='POST':
+        form=Notesform(request.POST)  #by this the data will be save in the form
+        if form.is_valid():
+            notes=Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+            messages.success(request,'Successfully  added Notes by {}'.format(request.user.username))
+    else:    
+        form=Notesform()
     notes=Notes.objects.filter(user=request.user)
     context={'notes':notes,'form':form}
     return render(request,'dashboard/notes.html',context)
+
+
+def delete_note(request,pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+
+
+class Notesdetailedview(generic.DetailView):   #generic views is a class but the other views are the functions
+    model = Notes
+
+
+def homework(request):
+    homeworks=Homework.objects.filter(user=request.user)
+    context={'homeworks':homeworks}
+    return render(request,'dashboard/homework.html',context)
