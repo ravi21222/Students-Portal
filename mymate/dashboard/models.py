a8eaf2dbbab5209764eaa96856d89481ab09ed8d
shreyansh2310 <sg275527109@gmail.com>
2022-01-26 15:00:04 +0530

    added changes in notes section

--- mymate/dashboard/models.py
+++ mymate/dashboard/models.py
@@ -1,3 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Notes(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Notes'
+        verbose_name_plural = 'Notes'
--- mymate/dashboard/views.py
+++ mymate/dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from . forms import *
 # Create your views here.
+def home(request):
+    return render(request,'dashboard/home.html')
+
+
+def notes(request):
+    form=Notesform()
+    notes=Notes.objects.filter(user=request.user)
+    context={'notes':notes,'form':form}
+    return render(request,'dashboard/notes.html',context)
